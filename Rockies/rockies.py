7daa0a70a57adb3d4cb8ed82617917c54b5a1a9c
Evan Hosmer <38225896+evanhosmer@users.noreply.github.com>
2018-09-10 10:30:12 -0600

    Add files via upload

---
+++ Rockies/rockies.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import os
+import urllib.request, json
+from urllib.parse import quote
+import csv
+
+def get_daily_data(date):
+    '''
+    INPUT: Date (example: 2018-05-09).
+    OUTPUT: Game data for the input date.
+    '''
+    year, month, day = date.split('-')
+    # Format url string with year, month, and day from input
+    dateurl = ('https://gd2.mlb.com/components/game/mlb/year_{}/month_{}/day_{}/grid.json'.format(year, month, day))
+
+    # Load in the json grid from the url
+    with urllib.request.urlopen(dateurl) as url:
+        data = json.loads(url.read().decode())
+
+    return data
+
+def get_game_data(grid, date):
+    '''
+    INPUT: JSON grid, desired date.
+    OUTPUT: CSV delimited file of game data for the specified date.
+    '''
+    # Access the required dictionary
+    games = grid['data']['games']['game']
+    year, month, day = date.split('-')
+
+    # Remove unnecessary information
+    for d in games:
+        del d['game_media']
+        del d['newsroom']
+
+    # Create a csv file to store the data with name reflecting the date. Data stored in data directory.
+    daily_game_data = open('/Users/evanhosmer/Desktop/Rockies/data/{}-{}-{}.csv'.format(year,month,day), 'w')
+
+    # create the csv writer object
+    csvwriter = csv.writer(daily_game_data)
+    count = 0
+
+    # iterate through list of dictionaries
+    for game in games:
+          if count == 0:
+              header = game.keys()
+              csvwriter.writerow(header)
+              count += 1
+
+          csvwriter.writerow(game.values())
+
+    daily_game_data.close()
+
+if __name__ == '__main__':
+
+    # Acquire the json grid.
+    data = get_daily_data('2018-05-09')
+
+    #Get game data and store in a csv file.
+    get_game_data(data,'2018-05-09')
